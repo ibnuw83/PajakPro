0b409a29abd9935ae899cf2e182abd0354087bb6
Ibnu Wibowo <ibnuw83@gmail.com>
2025-08-15 16:28:24 +0000

    apakah ini sudah pwa

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,7 @@
 import type {NextConfig} from 'next';
+import withPWA from '@ducanh2912/next-pwa';
+
+const isDevelopment = process.env.NODE_ENV === 'development';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -17,4 +20,8 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: 'public',
+  register: true,
+  disable: isDevelopment,
+})(nextConfig);
